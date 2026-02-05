f2063bf3b634fb7a9ba97b09d8fa01dbe9468fa8
Érik Schünemann <erik.correa@srconsulting.io>
2026-02-05 15:59:19 -0300

    create bat

---
+++ leitor-livros.bat
@@ -0,0 +1,10 @@
+@echo off
+title Leitor de Livros
+echo Iniciando Leitor de Livros...
+echo.
+echo Acesse: http://localhost:3000
+echo.
+echo Pressione Ctrl+C para fechar
+echo.
+node server.js
+pause
